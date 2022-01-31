617f990a8c899814e94c6613536b313e01bd5876
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-01-31 17:28:34 +0530

    Employees info

--- src/components/products/Main.js
+++ src/components/products/Main.js
@@ -1,18 +1,198 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
+import { nanoid } from "nanoid";
+import data from "../server/employe.json";
+import ReadOnlyRow from "./ReadOnlyRow";
+import EditableRow from "./EditableRow";
 
-function Main() {
+const App = () => {
+  const [contacts, setContacts] = useState(data);
+  const [addFormData, setAddFormData] = useState({
+    fullName: "",
+    age: "",
+    city: "",
+    gender: "",
+    salary: "",
+  });
+
+  const [editFormData, setEditFormData] = useState({
+    fullName: "",
+    age: "",
+    city: "",
+    gender: "",
+    salary: "",
+  });
+
+  const [editContactId, setEditContactId] = useState(null);
+
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...addFormData };
+    newFormData[fieldName] = fieldValue;
+
+    setAddFormData(newFormData);
+  };
+
+  const handleEditFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...editFormData };
+    newFormData[fieldName] = fieldValue;
+
+    setEditFormData(newFormData);
+  };
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newContact = {
+      id: nanoid(),
+      fullName: addFormData.fullName,
+      age: addFormData.age,
+      city: addFormData.city,
+      gender: addFormData.gender,
+      salary: addFormData.salary,
+    };
+
+    const newContacts = [...contacts, newContact];
+    setContacts(newContacts);
+  };
+
+  const handleEditFormSubmit = (event) => {
+    event.preventDefault();
+
+    const editedContact = {
+      id: editContactId,
+      fullName: editFormData.fullName,
+      age: editFormData.age,
+      city: editFormData.city,
+      gender: editFormData.gender,
+      salary: editFormData.salary,
+    };
+
+    const newContacts = [...contacts];
+
+    const index = contacts.findIndex((contact) => contact.id === editContactId);
+
+    newContacts[index] = editedContact;
+
+    setContacts(newContacts);
+    setEditContactId(null);
+  };
+
+  const handleEditClick = (event, contact) => {
+    event.preventDefault();
+    setEditContactId(contact.id);
+
+    const formValues = {
+      fullName: contact.fullName,
+      age: contact.age,
+      city: contact.city,
+      gender: contact.gender,
+      salary: contact.salary,
+    };
+
+    setEditFormData(formValues);
+  };
+
+  const handleCancelClick = () => {
+    setEditContactId(null);
+  };
+
+  const handleDeleteClick = (contactId) => {
+    const newContacts = [...contacts];
+
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    newContacts.splice(index, 1);
+
+    setContacts(newContacts);
+  };
+
   return (
-    <>
-      <div className="album   py-3">
-        <div className="container">
-          <div
-            className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"
-            style={{ height: "587px" }}
-          ></div>
-        </div>
-      </div>
-    </>
-  );
-}
+    <div className="app-container">
+      <form onSubmit={handleEditFormSubmit}>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Age</th>
+              <th scope="col">City</th>
+              <th scope="col">Gender</th>
+              <th scope="col">Salary</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((contact) => (
+              <Fragment>
+                {editContactId === contact.id ? (
+                  <EditableRow
+                    editFormData={editFormData}
+                    handleEditFormChange={handleEditFormChange}
+                    handleCancelClick={handleCancelClick}
+                  />
+                ) : (
+                  <ReadOnlyRow
+                    contact={contact}
+                    handleEditClick={handleEditClick}
+                    handleDeleteClick={handleDeleteClick}
+                  />
+                )}
+              </Fragment>
+            ))}
+          </tbody>
+        </table>
+      </form>
 
-export default Main;
+      <h2>Add a Contact</h2>
+      <form onSubmit={handleAddFormSubmit}>
+        <input
+          type="text"
+          name="fullName"
+          required="required"
+          placeholder="Enter a name..."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="number"
+          name="age"
+          required="required"
+          placeholder="Enter an age..."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="text"
+          name="city"
+          required="required"
+          placeholder="Enter a city..."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="text"
+          name="gender"
+          required="required"
+          placeholder="Enter a gender..."
+          onChange={handleAddFormChange}
+        />
+
+        <input
+          type="number"
+          name="salary"
+          required="required"
+          placeholder="Enter a salary..."
+          onChange={handleAddFormChange}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/products/ReadOnlyRow.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ReadOnlyRow = ({ contact, handleEditClick, handleDeleteClick }) => {
+  return (
+    <tr>
+      <td>{contact.fullName}</td>
+      <td>{contact.age}</td>
+      <td>{contact.city}</td>
+      <td>{contact.gender}</td>
+      <td>{contact.salary}</td>
+      <td>
+        <button
+          type="button"
+          onClick={(event) => handleEditClick(event, contact)}
+        >
+          Edit
+        </button>
+        <button type="button" onClick={() => handleDeleteClick(contact.id)}>
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default ReadOnlyRow;
